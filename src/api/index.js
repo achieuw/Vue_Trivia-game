cb16b48c8dccd7ef36e30558b22922bbe1e65175
edwineliasson98 <edwin.eliasson.git@gmail.com>
2022-01-21 16:33:37 +0100

    FEAT: adds api methods for users & categories

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "http://bestest-trivia-api.herokuapp.com/trivia"
+export const API_KEY = "6jpcXNp8F8AoBw44eN67H6h1apUgETuzRzl7QIcSnWILdzwoq78ve3FMNUnbON7h"
+export const QUESTION_URL = "https://opentdb.com/api"
