8e20c12332099c762faf46d84c4ca79207b012e8
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-23 16:24:12 +0100

    FEAT: add question api and question view

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import StartView from './views/StartView.vue'
+import QuestionView from './views/QuestionView.vue'
 
 const routes = [
   {
     path: '/',
     component: StartView
+  },
+  {
+    path: '/questions',
+    component: QuestionView
   }
 ]
 
---
+++ src/views/QuestionView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Question from '../components/Question.vue';
+import { apiGetQuestions } from '../api/questions';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+    const { error, response } = await apiGetQuestions(10);
+})
+
+</script>
+
+<template>
+    <h1>Questions</h1>
+    <!-- <Question v-for="(question, index) in response" :key="index" :questionNumber="index"  /> -->
+</template>
+
+<style>
+</style>
