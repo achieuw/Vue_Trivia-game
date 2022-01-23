feae0f8237a231451a2d8184586219da43ea3113
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-23 16:46:56 +0100

    FEAT: adds question to questionview

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import ViewButton from '../components/ViewButton.vue'
+import Question from '../components/Question.vue';
 
 const router = useRouter()
+
+
 
 const onClickView = () => {
   router.push('Result')
@@ -10,8 +13,9 @@ const onClickView = () => {
 </script>
 
 <template>
-<h1>Question View</h1>
-<ViewButton class="bg-emerald-400 w-28 rounded-md p-2 hover:bg-emerald-500" 
-      buttonText="Go to result" 
-      @onClick="onClickView"/>
+  <h1>Question View</h1>
+  <Question :questionNumber="1" />
+  <ViewButton class="bg-emerald-400 w-28 rounded-md p-2 hover:bg-emerald-500" 
+        buttonText="Go to result" 
+        @onClick="onClickView"/>
 </template>
