3a64189a34232ed3fd14438273ef64fbe426f5ce
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-25 09:51:12 +0100

    FEAT: adds resultscore component

---
+++ src/components/ResultScore.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+
+const store = useStore()
+
+const score = computed(() => store.state.score)
+const highScore = computed(() => store.state.highScore)
+
+</script>
+
+<template>
+    <div class="flex flex-col text-2xl">
+        <h1> HighScore: {{ highScore }} </h1>
+        <h1> Score: {{ score }} </h1>
+    </div>
+</template>
