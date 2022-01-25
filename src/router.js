0be637050497c50c596295897c8f77c7022e99c8
RasmusMoller <rasmus.moller@se.experis.com>
2022-01-25 14:28:03 +0100

    FIX: Authguard routing when no user in store

--- src/router.js
+++ src/router.js
@@ -1,7 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from './store'
 import StartView from './views/StartView.vue'
 import QuestionView from './views/QuestionView.vue'
 import ResultView from './views/ResultView.vue'
+
+const authGuard = (to, from, next) => {
+  if(store.state.user === "") {
+    next('/')
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
@@ -10,11 +19,13 @@ const routes = [
   },
   {
     path: '/Questions',
-    component: QuestionView
+    component: QuestionView,
+    beforeEnter: authGuard
   },
   {
     path: '/Result',
-    component: ResultView
+    component: ResultView,
+    beforeEnter: authGuard
   }
 ]
 
