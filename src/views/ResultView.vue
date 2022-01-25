21e2d884aaac7094e3bc671151182f3b146f1fdb
Edwin Eliasson <edwin.eliasson.git@gmail.com>
2022-01-25 09:53:05 +0100

    STYLE: styles result score

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -2,6 +2,7 @@
 import { useRouter } from 'vue-router';
 import ViewButton from '../components/ViewButton.vue'
 import ResultList from '../components/ResultList.vue';
+import ResultScore from '../components/ResultScore.vue';
 
 const router = useRouter()
 
@@ -16,13 +17,15 @@ const onClickQuestionView = () => {
 <template>
 <h1 class="text-center text-5xl">Result View</h1>
 <ResultList />
-<div class="fixed flex top-1/3 right-0 bg-[#FCAA67f9] p-3">
-  <!-- put score here? -->
-  <ViewButton 
-        buttonText="Go to start" 
-        @onClick="onClickStartView"/>
-  <ViewButton 
-        buttonText="Play again" 
-        @onClick="onClickQuestionView"/>
+<div class="fixed flex flex-col gap-2 justify-evenly top-1/3 right-0 bg-[#FCAA67f9] p-3 w-72 h-40 shadow-xl">
+  <ResultScore />
+  <div class="flex gap-2">
+    <ViewButton 
+          buttonText="Go to start" 
+          @onClick="onClickStartView"/>
+    <ViewButton 
+          buttonText="Play again" 
+          @onClick="onClickQuestionView"/>
+  </div>
 </div>
 </template>
